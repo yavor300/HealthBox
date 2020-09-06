d8bac831792bb585ac376a0c42fcc26ef55dd09d
= <yavor300@gmail.com>
2020-09-06 18:25:17 +0300

    Initial commit

---
+++ src/main/java/web/healthbox/HealthboxApplication.java
@@ -0,0 +1,13 @@
+package web.healthbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/web/healthbox/HealthboxApplicationTests.java
@@ -0,0 +1,13 @@
+package web.healthbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
