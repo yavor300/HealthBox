5ecddd84862e917aae8574d9d542e56ba6acf846
Yavor <yavor300@gmail.com>
2021-03-14 18:44:05 +0200

    Added scheduler for clearing the log every six months.

--- src/main/java/project/healthbox/HealthboxApplication.java
+++ src/main/java/project/healthbox/HealthboxApplication.java
@@ -2,10 +2,11 @@ package project.healthbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class HealthboxApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(HealthboxApplication.class, args);
     }
